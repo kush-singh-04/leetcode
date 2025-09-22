608ff5c224cbe7db4ab9575d169edab09fad702b
kush-singh-04 <kushs3416@gmail.com>
2025-09-22 19:42:05 +0530

    Time: 8 ms (99.28%), Space: 57.1 MB (38.86%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,40 @@
+ 
+class Solution {
+    public ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        ListNode temp1=list1;
+        ListNode temp2=list2;
+        while(temp1!=null && temp2!=null){
+            if(temp1.val<=temp2.val){
+                temp.next=temp1;
+                temp1=temp1.next;
+            }
+            else{
+                temp.next=temp2;
+                temp2=temp2.next;
+            }
+            temp=temp.next;
+        }
+        if(temp1==null) temp.next=temp2;
+        else temp.next=temp1;
+        return dummy.next;
+    }    
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode firsthalf = head;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode secondhalf=slow.next;
+        slow.next=null;
+        firsthalf=sortList(firsthalf);
+        secondhalf=sortList(secondhalf);
+        ListNode ans = merge(firsthalf,secondhalf);
+        return ans;
+        
+    }
+}
