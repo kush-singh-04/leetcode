3685a516ae0b2de8ecf4bcbdad6f045be3dc0477
kush-singh-04 <kushs3416@gmail.com>
2025-09-23 14:48:14 +0530

    Time: 1 ms (98.18%), Space: 44.5 MB (96.35%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.java
+++ 1168-duplicate-zeros/1168-duplicate-zeros.java
@@ -1,15 +1,34 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
         int n = arr.length;
-        for(int i =0;i<n-1;i++){
-            if(arr[i]==0){
-                for(int j=n-1;j>i;j--){
-                    arr[j]=arr[j-1];
+        int temp[] = new int[n];
+        int j = 0;
+        int i = 0;
+       while(i<n && j<n){
+            if(arr[i]!=0){
+                temp[j] = arr[i];
+                j++;
+                
+                
+            }else{
+                temp[j] = 0;
+                if(j+1<n){
+                    temp[j+1] = 0;
+                    
+
                 }
-                i++;
+                j+=2;
+               
             }
+
+            i++;
+            
             
 
+        }
+
+        for (int k = 0; k < n; k++) {
+            arr[k] = temp[k];
         }
 
 
