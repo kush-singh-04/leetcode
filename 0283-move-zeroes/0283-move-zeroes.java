0c211f9b8738a48e9385e86ae38afb3337912f24
kush-singh-04 <kushs3416@gmail.com>
2025-12-15 11:48:17 +0530

    Time: 117 ms (5.01%), Space: 47.7 MB (36.48%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int noz = 0;
+        int n = arr.length;
+        for(int ele : arr){
+            if(ele==0) noz++;
+        }
+        for(int x=0;x<noz;x++){
+            for(int i=0;i<n-1-x;i++){
+                if (arr[i]==0){
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+            }
+        }
+        
+    }
+}
