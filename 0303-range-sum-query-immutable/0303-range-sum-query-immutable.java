b75ff258612d4a3549336efd189cd58237365ad1
kush-singh-04 <kushs3416@gmail.com>
2026-07-19 10:15:31 +0530

    Time: 7 ms (99.99%), Space: 47.8 MB (25.87%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -2,15 +2,16 @@ class NumArray {
     int[] arr;
 
     public NumArray(int[] nums) {
-        arr = Arrays.copyOf(nums,nums.length);// deep copy
-        for(int i=1;i<nums.length;i++){
-            arr[i] +=arr[i-1];
+        arr = Arrays.copyOf(nums,nums.length);
+        int n = arr.length;
+        for(int i=1;i<n;i++){
+            arr[i]+=arr[i-1];
         }
     }
     
     public int sumRange(int left, int right) {
-       if(left==0) return arr[right];
-       return arr[right] - arr[left-1]; 
+        if(left==0) return arr[right];
+        return arr[right] - arr[left-1];
     }
 }
 
