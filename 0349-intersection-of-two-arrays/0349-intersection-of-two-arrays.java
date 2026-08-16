cfd010a0dbdbf0b711951dd8945402f054392b15
kush-singh-04 <kushs3416@gmail.com>
2026-08-16 09:53:03 +0530

    Time: 6 ms (20.75%), Space: 45.2 MB (25.04%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,30 +1,36 @@
-class Solution {
-    public int[] intersection(int[] nums1, int[] nums2) {
-
-        int n = nums1.length;
-        int m = nums2.length;
-
-        int[] temp = new int[Math.min(n, m)];
-
-        HashSet<Integer> set = new HashSet<>();
-
-        for (int num : nums1) {
-            set.add(num);
-        }
-
-        int k = 0;
-
-        for (int i = 0; i < m; i++) {
-
-            if (set.contains(nums2[i])) {
-                temp[k] = nums2[i];
-                k++;
-
-                // remove so duplicate is not added
-                set.remove(nums2[i]);
+class Solution { 
+    public int[] intersection(int[] nums1, int[] nums2) { 
+        Arrays.sort(nums1); 
+        Arrays.sort(nums2); 
+        
+        int n = nums1.length; 
+        int m = nums2.length; 
+        
+        HashSet<Integer> set = new HashSet<>(); 
+        
+        int i=0,j=0; 
+        
+        while(i<n && j<m){ 
+            if(nums1[i] == nums2[j]){ 
+                set.add(nums1[i]);
+                i++;
+                j++;
             }
+            else if(nums1[i] < nums2[j]){
+                i++;
+            }
+            else{
+                j++;
+            }
+        } 
+        
+        int[] ans = new int[set.size()];
+        int k=0;
+        
+        for(int ele : set){
+            ans[k++] = ele;
         }
-
-        return Arrays.copyOf(temp, k);
-    }
+        
+        return ans; 
+    } 
 }
