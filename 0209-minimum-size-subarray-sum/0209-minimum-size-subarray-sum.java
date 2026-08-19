58b7a26a2e88d8cf21e0315d96d52e1070a57e3f
kush-singh-04 <kushs3416@gmail.com>
2026-08-19 11:49:13 +0530

    Time: 2 ms (21.18%), Space: 62.2 MB (99.91%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,47 +1,23 @@
-class Solution { 
-    public int minSubArrayLen(int target, int[] nums) { 
-        int n = nums.length; 
-        int minlength = Integer.MAX_VALUE; 
-        int sum = 0; 
-
-        for(int i=1;i<n;i++){ 
-            nums[i] += nums[i-1]; 
-        } 
-
-        int i=0,j=0; 
-
-        while(j<n){ 
-
-            if(i==0){ 
-                sum = nums[j]; 
-            } 
-            else{ 
-                sum = nums[j] - nums[i-1]; 
-            } 
-
-            while(i<=j && sum>=target){ 
-                int length = j-i+1; 
-                minlength = Math.min(length,minlength); 
-
-                i++; 
-
-                if(i<=j){ 
-                    if(i==0){ 
-                        sum = nums[j]; 
-                    } 
-                    else{ 
-                        sum = nums[j] - nums[i-1]; 
-                    } 
-                } 
-            } 
-
-            j++; 
-        } 
-
-        if(minlength == Integer.MAX_VALUE){ 
-            return 0; 
-        } 
-
-        return minlength; 
-    } 
+class Solution {
+    public int minSubArrayLen(int target, int[] arr) {
+        int n = arr.length,minlen = Integer.MAX_VALUE;
+        int i =0,j=0,sum=0;
+        while(j<n && sum< target){
+            sum += arr[j++];
+        }
+        j--;
+        // sliding window
+        while(i<n && j<n){
+            int len = j-i+1;
+            if(sum>=target)minlen = Math.min(minlen,len);
+            sum -=arr[i];
+            i++;j++;
+            while(j<n && sum<target){
+                sum +=arr[j++];
+            }
+            j--;
+        }
+        if(minlen==Integer.MAX_VALUE) return 0;
+        return minlen;
+    }
 }
