38a5b8c15ce209df52f007f1cce153f65ac861df
kush-singh-04 <kushs3416@gmail.com>
2026-09-01 12:24:52 +0530

    Time: 2 ms (81.59%), Space: 43.5 MB (84.45%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,25 +1,30 @@
-class Solution { 
-    public String removeDuplicateLetters(String s) { 
-        int n = s.length(); 
-        boolean visited[] = new boolean[26];
-        int lastindex[]=new int[26];
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int n = s.length();
+        boolean[] visited = new boolean[26];
+        int[] lastindex = new int[26];
         for(int i=0;i<n;i++){
             lastindex[s.charAt(i)-'a']=i;
         }
         Stack<Integer> stack = new Stack<>();
         for(int i=0;i<n;i++){
-            int c = s.charAt(i)-'a';
-            if(visited[c]) continue;
-            visited[c]=true;
-            while(!stack.isEmpty()&&stack.peek()>c&&lastindex[stack.peek()]>i){
-                visited[stack.pop()]=false;
+           int c = s.charAt(i)-'a';
+           if(visited[c]) continue;
+           visited[c]=true;
+           while(!stack.isEmpty()&&stack.peek()>c&&lastindex[stack.peek()]>i){
+             
+                int x = stack.pop();
+                visited[x] = false;
             }
             stack.push(c);
+
         }
         StringBuilder ans = new StringBuilder();
-        for(int l:stack){
+        for(int l: stack){
             ans.append((char)(l+'a'));
         }
         return ans.toString();
-    } 
+
+        
+    }
 }
